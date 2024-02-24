8708ddfed3c51239c07192ba4a10097f5c82426e
kamendov-maxim <kamendov.maksim@icloud.com>
2024-02-24 10:22:55 +0300

    Создал ветку

---
+++ Bor/Program.cs
@@ -0,0 +1,14 @@
+﻿class Bor
+{
+    public bool Add(string element)
+    {
+        return true;
+    }
+
+    public bool Contains(string element)
+    {
+        return true;
+    }
+
+    public int Size;
+}
