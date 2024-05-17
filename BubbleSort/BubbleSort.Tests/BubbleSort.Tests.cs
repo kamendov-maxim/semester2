1907510d18dccebfb46ece6cae461b876d2f51d0
kamendov-maxim <kamendov.maksim@icloud.com>
2024-05-17 15:12:12 +0300

    fix tests

--- BubbleSort/BubbleSort.Tests/BubbleSort.Tests.cs
+++ BubbleSort/BubbleSort.Tests/BubbleSort.Tests.cs
@@ -5,7 +5,7 @@ public class Tests
 {
     private readonly int[] intArray = [6, 3, 89, 54, 2, 5, 2, 0, 1, 4, 7, 34, 76, 2, 6];
     private readonly string[] stringArray = ["ddd", "aa", "c", "bbbb"];
-    private readonly List<List<int>> listOfLists = [[2, 2], [1], [4, 4, 4, 4], [3, 3, 3, 3]];
+    private readonly List<int>[] arrayOfLists = [[2, 2], [1], [4, 4, 4, 4], [3, 3, 3]];
 
     [Test]
     public void TestBubbleSortWithStringsAndComparerByLength()
@@ -15,12 +15,12 @@ public class Tests
         Assert.That(stringArray, Is.EqualTo(expectedResult));
     }
 
-    public void TestBubbleSortOnListOfLIsts()
+    [Test]
+    public void TestBubbleSortOnArrayOfLists()
     {
-        List<List<int>> expectedResult = [[1], [2, 2], [3, 3, 3, 3], [4, 4, 4, 4]];
-        Sorting.BubbleSort<List<int>>(listOfLists, Comparer<List<int>>.Create((List<int> a, List<int> b) => a.Count - b.Count));
-        Assert.That(listOfLists, Is.EqualTo(expectedResult));
-
+        List<int>[] expectedResult = [[1], [2, 2], [3, 3, 3], [4, 4, 4, 4]];
+        Sorting.BubbleSort<List<int>>(arrayOfLists, Comparer<List<int>>.Create((List<int> a, List<int> b) => a.Count - b.Count));
+        Assert.That(arrayOfLists, Is.EqualTo(expectedResult));
     }
 
     [Test]
