f3ec032745ee83adbf80c2e2527ce1bf2f69bc9a
kamendov-maxim <kamendov.maksim@icloud.com>
2024-05-23 16:55:19 +0300

    fix ci

--- RunningButton/RunningButton/Program.cs
+++ RunningButton/RunningButton/Program.cs
@@ -8,6 +8,11 @@ namespace RunningButton
         [STAThread]
         static void Main()
         {
+            if (!System.OperatingSystem.IsWindows())
+            {
+                Console.WriteLine("Program is only supported on Windows");
+                System.Environment.Exit(0);
+            }
             ApplicationConfiguration.Initialize();
             Application.Run(new RunningButtonForm());
         }
